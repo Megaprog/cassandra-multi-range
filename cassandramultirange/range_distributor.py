5142a140fb03f25659c893dda4d00e94abd96305
Tomas Shestakov <shestakovte@gmail.com>
2021-08-15 18:03:28 +0300

    in case of Exception in one process set Event to notify others

--- cassandramultirange/range_distributor.py
+++ cassandramultirange/range_distributor.py
@@ -1,11 +1,20 @@
-from multiprocessing import Pool
-from typing import Callable, TypeVar, Iterator
+from multiprocessing import Pool, Event
+from typing import Callable, TypeVar, Iterator, Union
 
 from token_range import split_long, token_range_inclusive
 
 T = TypeVar('T')
 
 
-def distribute_by_range(pool: Pool, n: int, func: Callable[[tuple[int, int, int]], int]) -> Iterator[int]:
+def distribute_by_range(pool: Pool, n: int, func: Callable[[int, int, int, Event], T]) -> list[Union[T, Exception]]:
     splits = split_long(n)
-    return pool.imap_unordered(func, ((i, *token_range_inclusive(splits, i)) for i in range(n)))
+    event = Event()
+
+    def wrapper(index: int, low_inc: int, high_inc: int):
+        try:
+            return func(index, low_inc, high_inc, event)
+        except Exception as e:
+            event.set()
+            return e
+
+    return pool.starmap(wrapper, ((i, *token_range_inclusive(splits, i)) for i in range(n)))
--- tests/test_range_distributor.py
+++ tests/test_range_distributor.py
@@ -1,3 +1,4 @@
+from multiprocessing import Event
 from multiprocessing.pool import ThreadPool
 
 from range_distributor import distribute_by_range
@@ -5,18 +6,38 @@ from token_range import LONG_MIN_VALUE, LONG_MAX_VALUE
 
 
 def test_distribute_by_range():
-    def validate_range(args):
-        index, low_inc, high_inc = args
+    def validate_range(index: int, low_inc: int, high_inc: int, event: Event):
         if index == 0:
             assert low_inc == LONG_MIN_VALUE
             assert high_inc == -1
+            return "a"
         else:
             assert low_inc == 0
             assert high_inc == LONG_MAX_VALUE
-
-        return index
+            return "b"
 
     with ThreadPool(2) as thread_pool:
-        result = set(distribute_by_range(thread_pool, 2, validate_range))
+        result = distribute_by_range(thread_pool, 2, validate_range)
 
-    assert result == {0, 1}
+    assert result == ["a", "b"]
+
+
+def test_distribute_by_range_exception():
+    def validate_range(index: int, low_inc: int, high_inc: int, event: Event):
+        if index == 0:
+            assert low_inc == LONG_MIN_VALUE
+            assert high_inc == -1
+            event.wait(60)
+            return "a"
+        else:
+            assert low_inc == 0
+            assert high_inc == LONG_MAX_VALUE
+            raise ValueError("b")
+
+    with ThreadPool(2) as thread_pool:
+        result = distribute_by_range(thread_pool, 2, validate_range)
+
+    assert len(result) == 2
+    assert result[0] == "a"
+    assert isinstance(result[1], ValueError)
+    assert result[1].args[0] == "b"
