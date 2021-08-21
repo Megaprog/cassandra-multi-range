c756154a76fca0f39eeb49e042e1a2b0435c6977
Tomas Shestakov <shestakovte@gmail.com>
2021-08-21 21:23:33 +0300

    range_distributor was done

--- cassandramultirange/range_distributor.py
+++ cassandramultirange/range_distributor.py
@@ -1,20 +1,22 @@
-from multiprocessing import Pool, Event
-from typing import Callable, TypeVar, Iterator, Union
+from multiprocessing import Pool, Event, Manager
+from typing import Callable, TypeVar, Union
 
 from token_range import split_long, token_range_inclusive
 
 T = TypeVar('T')
 
 
+def _exception_handler_wrapper(index: int, low_inc: int, high_inc: int, event: Event,
+                               func: Callable[[int, int, int, Event], T]):
+    try:
+        return func(index, low_inc, high_inc, event)
+    except Exception as e:
+        event.set()
+        return e
+
+
 def distribute_by_range(pool: Pool, n: int, func: Callable[[int, int, int, Event], T]) -> list[Union[T, Exception]]:
     splits = split_long(n)
-    event = Event()
-
-    def wrapper(index: int, low_inc: int, high_inc: int):
-        try:
-            return func(index, low_inc, high_inc, event)
-        except Exception as e:
-            event.set()
-            return e
-
-    return pool.starmap(wrapper, ((i, *token_range_inclusive(splits, i)) for i in range(n)))
+    with Manager() as manager:
+        event = manager.Event()
+        return pool.starmap(_exception_handler_wrapper, ((i, *token_range_inclusive(splits, i), event, func) for i in range(n)))
--- tests/test_range_distributor.py
+++ tests/test_range_distributor.py
@@ -1,4 +1,4 @@
-from multiprocessing import Event
+from multiprocessing import Event, Pool
 from multiprocessing.pool import ThreadPool
 
 from range_distributor import distribute_by_range
@@ -6,38 +6,58 @@ from token_range import LONG_MIN_VALUE, LONG_MAX_VALUE
 
 
 def test_distribute_by_range():
-    def validate_range(index: int, low_inc: int, high_inc: int, event: Event):
-        if index == 0:
-            assert low_inc == LONG_MIN_VALUE
-            assert high_inc == -1
-            return "a"
-        else:
-            assert low_inc == 0
-            assert high_inc == LONG_MAX_VALUE
-            return "b"
-
     with ThreadPool(2) as thread_pool:
-        result = distribute_by_range(thread_pool, 2, validate_range)
+        result = distribute_by_range(thread_pool, 2, _validate_range)
+
+    assert result == ["a", "b"]
+
+
+def test_distribute_by_range_multiprocessing():
+    with Pool(2) as pool:
+        result = distribute_by_range(pool, 2, _validate_range)
 
     assert result == ["a", "b"]
 
 
 def test_distribute_by_range_exception():
-    def validate_range(index: int, low_inc: int, high_inc: int, event: Event):
-        if index == 0:
-            assert low_inc == LONG_MIN_VALUE
-            assert high_inc == -1
-            event.wait(60)
-            return "a"
-        else:
-            assert low_inc == 0
-            assert high_inc == LONG_MAX_VALUE
-            raise ValueError("b")
-
     with ThreadPool(2) as thread_pool:
-        result = distribute_by_range(thread_pool, 2, validate_range)
+        result = distribute_by_range(thread_pool, 2, _validate_range_exception)
 
     assert len(result) == 2
     assert result[0] == "a"
     assert isinstance(result[1], ValueError)
     assert result[1].args[0] == "b"
+
+
+def test_distribute_by_range_multiprocessing_exception():
+    with Pool(2) as pool:
+        result = distribute_by_range(pool, 2, _validate_range_exception)
+
+    assert len(result) == 2
+    assert result[0] == "a"
+    assert isinstance(result[1], ValueError)
+    assert result[1].args[0] == "b"
+
+
+def _validate_range(index: int, low_inc: int, high_inc: int, event: Event):
+    if index == 0:
+        assert low_inc == LONG_MIN_VALUE
+        assert high_inc == -1
+        return "a"
+    else:
+        assert low_inc == 0
+        assert high_inc == LONG_MAX_VALUE
+        return "b"
+
+
+def _validate_range_exception(index: int, low_inc: int, high_inc: int, event: Event):
+    if index == 0:
+        assert low_inc == LONG_MIN_VALUE
+        assert high_inc == -1
+        event.wait(60)
+        return "a"
+    else:
+        assert low_inc == 0
+        assert high_inc == LONG_MAX_VALUE
+        raise ValueError("b")
+
